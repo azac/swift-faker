a1af68c208c5e63e73a1301e678b5935bffe6006
azac <azac@azac.pl>
2014-07-24 14:21:37 +0200

    example corrected for XCode 6 beta 4

--- Faker/ViewController.swift
+++ Faker/ViewController.swift
@@ -3,33 +3,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var avatarImage: UIImageView
-    
-    @IBOutlet var label: UILabel
-    
+    @IBOutlet var avatarImage: UIImageView!
+
+    @IBOutlet var label: UILabel!
+
+
     @IBAction func klik(sender: AnyObject) {
-        
+
         var fakeGuy = Faker()
-        
+
         label.text=fakeGuy.fullDescription
-    
+
         avatarImage.image = UIImage(named:"")
-    
+
+
+
+
         dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-            var downloadedImage =  UIImage(data: NSData(contentsOfURL: NSURL(string:fakeGuy.photo)))
-            
-            self.avatarImage.image = downloadedImage
- 
+
+
+            let imgURL = NSURL.URLWithString(fakeGuy.photo);
+
+            var request: NSURLRequest = NSURLRequest(URL: imgURL)
+
+            var urlConnection: NSURLConnection = NSURLConnection(request: request, delegate: self)
+
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!,data: NSData!,error: NSError!) -> Void in
+                if !error? {
+
+                    var downloadedImage = UIImage(data: data)
+
+                    self.avatarImage.image = downloadedImage
+
+                }
+                else {
+                    println("Error: \(error.localizedDescription)")
+                }
+                })
+
+
+
+
         })
-        
-        
-        
-    
+
+
+
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
